2b5a0ee71aee44abad1c802c019b31a9315ea0fe
alfonsoroca <alfonsoroca@gmail.com>
2021-02-18 22:32:13 +0100

    Revisión y reescritura del código y documentación para el javadoc del tercer método, que sobrecarga el método restaNumReal

--- Calculadora/src/calculadora/Clase2Resta.java
+++ Calculadora/src/calculadora/Clase2Resta.java
@@ -23,8 +23,8 @@ public class Clase2Resta {
 	 * Con el fin de evitar que se usen números negativos en la operación, se utilizará
 	 * el valor absoluto de los números introducidos.
 	 * <br><br><b>Atención:</b> No se pueden usar números negativos en las operaciones
-	 * @param min Número real correspondiente con el minuendo
-	 * @param sust Número real correspondiente con el sustraendo
+	 * @param min Valor absoluto del número real correspondiente con el minuendo
+	 * @param sust Valor absoluto del número real correspondiente con el sustraendo
 	 * @return Retorna la resta del valor absoluto de los dos números introducidos según la fórmula: min - sust
 	 */
 
@@ -41,8 +41,8 @@ public class Clase2Resta {
 	 * Con el fin de evitar que se usen números negativos en la operación, se utilizará
 	 * el valor absoluto de los números introducidos.
 	 * <br><br><b>Atención:</b> No se pueden usar números negativos en las operaciones
-	 * @param min Número entero correspondiente con el minuendo
-	 * @param sust Número entero correspondiente con el sustraendo
+	 * @param min Valor absoluto del número entero correspondiente con el minuendo
+	 * @param sust Valor absoluto del número entero correspondiente con el sustraendo
 	 * @return Retorna la resta del valor absoluto de los dos números introducidos según la fórmula: min - sust
 	 */
 
@@ -53,21 +53,26 @@ public class Clase2Resta {
 		return min - sust;
 	}
 
-	// Siguiente revisión debe comenzar a partir de éste punto
 	/**
-	 * Método que retorna la resta de tres números reales.
+	 * Método que retorna la resta de tres números reales.</br></br>
+	 * Con el fin de evitar que se usen números negativos en la operación, se utilizará
+	 * el valor absoluto de los números introducidos.
 	 * <br><br><b>Atención:</b> No se pueden usar números negativos en las operaciones
-	 * @param min Número real correspondiente con el minuendo
-	 * @param sust1 Número real correspondiente con el primer sustraendo
-	 * @param sust2 Número real correspondiente con el segundo sustraendo
+	 * @param min Valor absoluto del número real correspondiente con el minuendo
+	 * @param sust1 Valor absoluto del número real correspondiente con el primer sustraendo
+	 * @param sust2 Valor absoluto del número real correspondiente con el segundo sustraendo
 	 * @return Retorna la resta de los tres números introducidos según la fórmula: (min - sust1) - sust2
 	 */
 
 	public static double restaNumReal(double min, double sust1, double sust2) {
+		min = Math.abs(min);
+		sust1 = Math.abs(sust1);
+		sust2 = Math.abs(sust2);
 
 		return (min - sust1) - sust2;
 	}
 	
+	// Siguiente revisión debe comenzar a partir de éste punto
 	/**
 	 * Método que acumula el resultado de los números introducidos a través del método acumular.<br><br>
 	 * Para la resta de un número, se debe indicar con un signo "-" delante del número introducido, en caso
