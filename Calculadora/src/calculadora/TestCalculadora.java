b888eda513f77363fe605cb18c5ba178c1ff420e
bokdelajungla <bokdelajungla@hotmail.com>
2021-02-21 19:37:29 +0100

    Parte de las pruebas de Clase4Cociente

--- Calculadora/src/calculadora/TestCalculadora.java
+++ Calculadora/src/calculadora/TestCalculadora.java
@@ -9,7 +9,32 @@ package calculadora;
 public class TestCalculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Prueba Clase4Cociente
+		Clase4Cociente c4test = new Clase4Cociente();
+		double real1 = 25.67;
+		double real2 = 4.78;
+		double ent1 = 6;
+		double ent2 = 2;
+		double zero = 0;
+		double nan = Double.NaN;
+		double masInf = Double.POSITIVE_INFINITY;
+		double menInf = Double.NEGATIVE_INFINITY;
+		//divisionReales
+		System.out.println("Prueba método divisionReales");
+		System.out.println(c4test.divisionReales(real1, real2));
+		//divisionEnteros
+		System.out.println("Prueba método divisionEnteros");
+		System.out.println(c4test.divisionEnteros(ent1, ent2));
+		//inverso
+		System.out.println("Prueba método divisionReales");
+		System.out.println(c4test.inverso(zero));
+		//raizcuadrada
+		System.out.println("Prueba método raízCuadrada");
+		System.out.println(c4test.raizCuadrada(nan));
+		
+
+				
 
 	}
 
