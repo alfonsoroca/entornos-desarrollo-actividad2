29f67e336da5b4a93248afde5248e540a7eb71c3
Cesar <cesarramirez1595@gmail.com>
2021-02-26 01:39:56 +0100

    Modificacion de metodos y documentacion

--- Calculadora/src/calculadora/Clase3Producto.java
+++ Calculadora/src/calculadora/Clase3Producto.java
@@ -13,16 +13,23 @@ public class Clase3Producto {
 	 * @param num2  numero entero que el segundo factor 
 	 * @return devuelve el producto de ambos factores
 	 * @since 24/01/21
+	 * 
+	 * <li>Si uno de los parametros es inferior a 0 no se calculará.</li> 
+	 * 
 	 */
 
-	public static int multiplicarEntero(int num1, int num2) {
-		
-		return num1 * num2;
+	public static void multiplicarEntero(int num1, int num2) {
+		if (num1 < 0 || num2 < 0) {
+			System.out.println("ERROR: Intentalo con numeros positivos");
+		} else {
+		System.out.println(num1 * num2); 
+		}
 	}
 
 	/**
 	 * <b>ATENION:</b> Numero grandes pueden provocar error si el resultado del
 	 * double supera los digitos que soporta
+	 * <li>Si uno de los parametros es inferior a 0 no se calculará.</li> 
 	 * 
 	 * @param num1  Numero real que es el primer factor
 	 * @param num2  Numero real que es el segundo factor
@@ -30,25 +37,39 @@ public class Clase3Producto {
 	 * @since 24/01/21
 	 */
 
-	public static double multiplicarDouble(double num1, double num2) {
+	public static void multiplicarDouble(double num1, double num2) {
 
-		return num1 * num2;
+		if (num1 < 0 || num2 < 0) {
+			System.out.println("ERROR: Intentalo con numeros positivos");
+		} else {
+		System.out.println(num1 * num2); 
+		}
 	}
 
 	/**
+	 * <li>Si uno de los parametros es inferior a 0 no se calculará.</li>
+	 * 
 	 * @param num1  Numero real que es el primer factor
 	 * @param num2  Numero real que es el segundo factor
 	 * @param num3  Numero real que es el tercer factor
 	 * @return devuelve el producto de los tres factores
 	 * @since 24/01/21
+	 * 
 	 */
 
-	public static double multiplicar3(double num1, double num2, double num3) {
+	public static void multiplicar3(double num1, double num2, double num3) {
 
-		return (num1 * num2) * num3;
+		if (num1 < 0 || num2 < 0 || num3 < 0) {
+			System.out.println("ERROR: Intentalo con numeros positivos");
+		} else {
+		System.out.println(num1 * (num2*num3)); 
+		}
 	}
 
 	/**
+	 * <li>La base no podrá ser inferior ni igual a 0</li>
+	 * <li>El exponente no podrá ser inferior ni igual a 0</li>
+	 * 
 	 * @param num1  Numero real que es la base
 	 * @param num2  Numero real que es el exponente
 	 * @return devuelve el resultado de la base elevada al exponente
@@ -57,9 +78,13 @@ public class Clase3Producto {
 	 * 
 	 */
 
-	public static double potencia(double num1, double num2) {
-
-		return Math.pow(num1, num2);
+	public static void potencia(double num1, double num2) {
+		if (num1 <= 0 || num2 <= 0 ) {
+			System.out.println("ERROR: Intentalo con numeros positivos mayores de 0");
+		} else {
+		System.out.println(Math.pow(num1, num2)); 
+		}
+		;
 
 		}
 }
--- Calculadora/src/calculadora/TestCalculadora.java
+++ Calculadora/src/calculadora/TestCalculadora.java
@@ -9,8 +9,8 @@ package calculadora;
 public class TestCalculadora {
 
 	public static void main(String[] args) {
-		
-		//Prueba Clase4Cociente
+
+		// Prueba Clase4Cociente
 		Clase4Cociente c4test = new Clase4Cociente();
 		double real1 = 25.67;
 		double real2 = 4.78;
@@ -20,40 +20,47 @@ public class TestCalculadora {
 		double nan = Double.NaN;
 		double masInf = Double.POSITIVE_INFINITY;
 		double menInf = Double.NEGATIVE_INFINITY;
-		//divisionReales
+		// divisionReales
 		System.out.println("Prueba metodo divisionReales");
 		System.out.println(c4test.divisionReales(real1, real2));
-		//divisionEnteros
+		// divisionEnteros
 		System.out.println("Prueba metodo divisionEnteros");
 		System.out.println(c4test.divisionEnteros(ent1, ent2));
-		//inverso
+		// inverso
 		System.out.println("Prueba metodo inverso");
 		System.out.println(c4test.inverso(zero));
 		System.out.println(c4test.inverso(menInf));
 		System.out.println(c4test.inverso(nan));
-		//raizcuadrada
+		// raizcuadrada
 		System.out.println("Prueba metodo raizCuadrada");
 		System.out.println(c4test.raizCuadrada(nan));
 
-		//Prueba Clase3Producto
-		System.out.println("\n" + "\n" + "Prueba multiplicarEntero:" + "\n" + Clase3Producto.multiplicarEntero(-5, 4));
-		System.out.println("Prueba multiplicarDouble: " + "\n" + Clase3Producto.multiplicarDouble(ent1, ent2) );
-		System.out.println("Prueba multiplicar3: " + "\n" + Clase3Producto.multiplicar3(real2, ent1, real1));
-		System.out.println("Prueba Potencia: " + "\n" + Clase3Producto.potencia(2, 5));
-		
-		//Prueba Clase2Resta		
+		// Prueba Clase3Producto
+		System.out.println("\n" + "\n" + "Prueba multiplicarEntero:");
+		Clase3Producto.multiplicarEntero(-2, 4);
+		System.out.println("Prueba multiplicarDouble: ");
+		Clase3Producto.multiplicarDouble(masInf, ent2);
+		System.out.println("Prueba multiplicar3: ");
+		Clase3Producto.multiplicar3(real2, ent1, real1);
+		System.out.println("Prueba Potencia: ");
+		Clase3Producto.potencia(1, 5);
+
+		// Prueba Clase2Resta
 		int int1 = 150000;
 		int int2 = 12356;
 		double double1 = 136531.536944;
 		double double2 = 478931.123568;
 		double double3 = 1323231.268944;
-		
+
 		System.out.println("\n----- Inicio de las pruebas de la Clase2Resta -----\n");
-		System.out.println("\nPrueba del m�todo restaNumReal con 2 par�metros: " + Clase2Resta.restaNumReal(double1, double2));
+		System.out.println(
+				"\nPrueba del m�todo restaNumReal con 2 par�metros: " + Clase2Resta.restaNumReal(double1, double2));
 		System.out.println("\nPrueba del m�todo restaNumEnt: " + Clase2Resta.restaNumEnt(int1, int2));
-		System.out.println("\nPrueba del m�todo restaNumReal con 3 par�metros: " + Clase2Resta.restaNumReal(double1, double2, double3));
+		System.out.println("\nPrueba del m�todo restaNumReal con 3 par�metros: "
+				+ Clase2Resta.restaNumReal(double1, double2, double3));
 		System.out.println("\nPrueba del m�todo acumular-----");
-		System.out.println("\nHas pulsado el 0 y has salido del acumulador. El total acumulado asciende a: " + Clase2Resta.acumular());
+		System.out.println("\nHas pulsado el 0 y has salido del acumulador. El total acumulado asciende a: "
+				+ Clase2Resta.acumular());
 		System.out.println("\n----- Fin de las pruebas de la Clase2Resta -----\n");
 	}
 
