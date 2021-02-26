57a701bd22226183df60cee39119c93fab169cd2
alfonsoroca <alfonsoroca@gmail.com>
2021-02-26 16:36:02 +0100

    Actualización de todos los comentarios según UTF8

--- Calculadora/src/calculadora/TestCalculadora.java
+++ Calculadora/src/calculadora/TestCalculadora.java
@@ -41,7 +41,7 @@ public class TestCalculadora {
 		System.out.println("Prueba multiplicar3: " + "\n" + Clase3Producto.multiplicar3(real2, ent1, real1));
 		System.out.println("Prueba Potencia: " + "\n" + Clase3Producto.potencia(real2, ent2));
 		
-		//Prueba Clase2Resta		
+		//Inicio pruebas Clase2Resta		
 		int int1 = 150000;
 		int int2 = 12356;
 		double double1 = 136531.536944;
@@ -49,12 +49,22 @@ public class TestCalculadora {
 		double double3 = 1323231.268944;
 		
 		System.out.println("\n----- Inicio de las pruebas de la Clase2Resta -----\n");
-		System.out.println("\nPrueba del mÈtodo restaNumReal con 2 par·metros: " + Clase2Resta.restaNumReal(double1, double2));
-		System.out.println("\nPrueba del mÈtodo restaNumEnt: " + Clase2Resta.restaNumEnt(int1, int2));
-		System.out.println("\nPrueba del mÈtodo restaNumReal con 3 par·metros: " + Clase2Resta.restaNumReal(double1, double2, double3));
-		System.out.println("\nPrueba del mÈtodo acumular-----");
+		
+		//Prueba del m√©todo restaNumReal con 2 parÔøΩmetros
+		System.out.println("\nPrueba del m√©todo restaNumReal con 2 par√°metros: " + Clase2Resta.restaNumReal(double1, double2));
+		
+		//Prueba del m√©todo restaNumEnt
+		System.out.println("\nPrueba del m√©todo restaNumEnt: " + Clase2Resta.restaNumEnt(int1, int2));
+		
+		//Prueba del m√©todo restaNumReal con 3 parÔøΩmetros
+		System.out.println("\nPrueba del m√©todo restaNumReal con 3 par√°metros: " + Clase2Resta.restaNumReal(double1, double2, double3));
+		
+		//Prueba del m√©todo acumular
+		System.out.println("\nPrueba del m√©todo acumular-----");
 		System.out.println("\nHas pulsado el 0 y has salido del acumulador. El total acumulado asciende a: " + Clase2Resta.acumular());
 		System.out.println("\n----- Fin de las pruebas de la Clase2Resta -----\n");
+		
+		//Fin pruebas Clase2Resta
 	}
 
 }
