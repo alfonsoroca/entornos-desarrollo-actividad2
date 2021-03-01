7097537cb93ec979999be6279098cf15b684db9d
alfonsoroca <alfonsoroca@gmail.com>
2021-03-01 07:54:48 +0100

    Mejoras en el código de las clases Clase2Resta y TestClase3Producto. Se añaden casos especiales relacionados con los valores extremos.

--- Calculadora/src/test/TestClase3Producto.java
+++ Calculadora/src/test/TestClase3Producto.java
@@ -16,122 +16,122 @@ import calculadora.Clase3Producto;
 class TestClase3Producto {
 
 	@Test
-	/**
-	 * Comprobando las multiplicaciones de dos números enteros
+	/*
+	 * Comprobando la multiplicación de dos números enteros
 	 */
 	void testMultiplicarEntero() {
 
 		int res = Clase3Producto.multiplicarEntero(-10, 156565);
 		int resEsperado = -1565650;
-		assertEquals(resEsperado, res, "El resultado esperado de la multiplicación de dos int falla");
+		assertEquals(resEsperado, res, "El resultado de la multiplicación de dos números enteros falla");
 
 	}
 
 	@Test
-	/**
-	 * Comprobando la multiplicación con infinito positivo
+	/*
+	 * Comprobando la multiplicación de cualquier número con infinito positivo
 	 */
 	void testMultiplicarDoubleInfinitoPositivo() {
 
 		double res = Clase3Producto.multiplicarDouble(1, Double.POSITIVE_INFINITY);
 		double resEsperado = Double.POSITIVE_INFINITY;
-		assertEquals(resEsperado, res, "El resultado de multiplicar un número por +infinito falla");
+		assertEquals(resEsperado, res, "El resultado de la multiplicación de cualquier número con infinito positivo falla");
 
 	}
 
 	@Test
-	/**
-	 * Comprobando la multiplicación con infinito negativo
+	/*
+	 * Comprobando la multiplicación de cualquier número con infinito negativo
 	 */
 	void testMultiplicarDoubleInfinitoNegativo() {
 
 		double res = Clase3Producto.multiplicarDouble(1, Double.NEGATIVE_INFINITY);
 		double resEsperado = Double.NEGATIVE_INFINITY;
-		assertEquals(resEsperado, res, "El resultado de multiplicar un número por -infinito falla");
+		assertEquals(resEsperado, res, "El resultado de la multiplicación de cualquier número con infinito negativo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la multiplicación de varios números con infinito positivo
 	 */
 	void testMultiplicarVariosDoubleInfinitoPositivo() {
 
 		double res = Clase3Producto.multiplicar3(1, 5, Double.POSITIVE_INFINITY);
 		double resEsperado = Double.POSITIVE_INFINITY;
-		assertEquals(resEsperado, res, "El resultado de multiplicar varios números por +infinito falla");
+		assertEquals(resEsperado, res, "El resultado de la multiplicación de varios números con infinito positivo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la multiplicación de varios números con infinito negativo
 	 */
 	void testMultiplicarVariosDoubleInfinitoNegativo() {
 
 		double res = Clase3Producto.multiplicar3(1, 5, Double.NEGATIVE_INFINITY);
 		double resEsperado = Double.NEGATIVE_INFINITY;
-		assertEquals(resEsperado, res, "El resultado de multiplicar varios números por -infinito falla");
+		assertEquals(resEsperado, res, "El resultado de la multiplicación de varios números con infinito negativo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la potencia de cero elevado a infinito positivo
 	 */
 	void testPotenciaCeroElevadoInfinitoPositivo() {
 
 		double res = Clase3Producto.potencia(0, Double.POSITIVE_INFINITY);
 		double resEsperado = 0;
-		assertEquals(resEsperado, res, "El resultado de elevar 0 a +infinito falla");
+		assertEquals(resEsperado, res, "El resultado de la potencia de cero elevado a infinito positivo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la potencia de cero elevado a infinito negativo
 	 */
 	void testPotenciaCeroElevadoInfinitoNegativo() {
 
 		double res = Clase3Producto.potencia(0, Double.NEGATIVE_INFINITY);
 		double resEsperado = Double.POSITIVE_INFINITY;
-		assertEquals(resEsperado, res, "El resultado de elevar 0 a -infinito falla");
+		assertEquals(resEsperado, res, "El resultado de la potencia de cero elevado a infinito negativo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la potencia de infinito positivo elevado a infinito positivo
 	 */
 	void testPotenciaInfinitoPositivoElevadoInfinitoPositivo() {
 
-		double resultado = Clase3Producto.potencia(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-		double resultadoEsperado = Double.POSITIVE_INFINITY;
-		assertEquals(resultado, resultadoEsperado);
+		double res = Clase3Producto.potencia(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+		double resEsperado = Double.POSITIVE_INFINITY;
+		assertEquals(resEsperado, res, "El resultado de la potencia de infinito positivo elevado a infinito positivo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la potencia de infinito positivo elevado a infinito negativo
 	 */
 	void testPotenciaInfinitoPositivoElevadoInfinitoNegativo() {
 
-		double resultado = Clase3Producto.potencia(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
-		double resultadoEsperado = 0.0;
-		assertEquals(resultado, resultadoEsperado);
+		double res = Clase3Producto.potencia(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
+		double resEsperado = 0.0;
+		assertEquals(resEsperado, res, "El resultado de la potencia de infinito positivo elevado a infinito negativo falla");
 
 	}
 
 	@Test
-	/**
+	/*
 	 * Comprobando la potencia de cero elevado a cero
 	 */
 	void testPotenciaCeroElevadoCero() {
 
-		double resultado = Clase3Producto.potencia(0, 0);
-		double resultadoEsperado = 1.0;
-		assertEquals(resultado, resultadoEsperado);
+		double res = Clase3Producto.potencia(0, 0);
+		double resEsperado = 1.0;
+		assertEquals(resEsperado, res, "El resultado de la potencia de cero elevado a cero falla");
 
 	}
 
