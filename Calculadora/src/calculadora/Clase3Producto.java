8c3d61278d79554836a0ce92cd3dd10663dc6fa7
Cesar <cesarramirez1595@gmail.com>
2021-03-02 17:14:22 +0100

    documentacion de test y mas visible

--- Calculadora/src/calculadora/Clase3Producto.java
+++ Calculadora/src/calculadora/Clase3Producto.java
@@ -24,21 +24,26 @@ public class Clase3Producto {
 
 	/** 
 	 * 
+	 * <li>Si un parametro se multiplica por infinito(positivo) el resultado sera Infinito(positivo) </li>
+	 * <li>Si un parametro se multiplica por infinito(negativo) el resultado sera Infinito(negativo) </li>
+	 * 
 	 * @param num1  Numero real que es el primer factor
 	 * @param num2  Numero real que es el segundo factor
 	 * @return devuelve el producto de los dos factores
 	 * @since 24/01/21
 	 * 
-	 * <li>Si un parametro se multiplica por infinito(positivo) el resultado sera Infinito(positivo) </li>
-	 * <li>Si un parametro se multiplica por infinito(negativo) el resultado sera Infinito(negativo) </li>
 	 */
 
 	public static double multiplicarDouble(double num1, double num2) {
+
 
 		return num1 * num2;
 	}
 
 	/**
+	 * 
+	 * <li>Si uno de los parametros es infinito(positivo) el resultado sera Infinito(positivo) </li>
+	 * <li>Si uno de los parametros es infinito(negativo) el resultado sera Infinito(negativo) </li>
 	 * 
 	 * @param num1  Numero real que es el primer factor
 	 * @param num2  Numero real que es el segundo factor
@@ -46,8 +51,6 @@ public class Clase3Producto {
 	 * @return devuelve el producto de los tres factores
 	 * @since 24/01/21
 	 *  
-	 * <li>Si uno de los parametros es infinito(positivo) el resultado sera Infinito(positivo) </li>
-	 * <li>Si uno de los parametros es infinito(negativo) el resultado sera Infinito(negativo) </li>
 	 */
 
 	public static double multiplicar3(double num1, double num2, double num3) {
@@ -57,15 +60,15 @@ public class Clase3Producto {
 
 	/**
 	 * 
+	 * <li>Cuando la base es elevada a Infinito(positivo) el resultado dará Infinito(positivo)</li>
+	 * <li>Cuando la base es Infinito(positivo) y el exponente es Infinito(negativo) el resultado será 0</li>
+	 * <li>Cuando la base es 0 y exponente 0 el resultado dará 1</li>
+	 * 
 	 * @param num1  Numero real que es la base
 	 * @param num2  Numero real que es el exponente
 	 * @return devuelve el resultado de la base elevada al exponente
 	 * @since 24/01/21
 	 * 
-	 * 
-	 * <li>Cuando la base es elevada a Infinito(positivo) el resultado dará Infinito(positivo)</li>
-	 * <li>Cuando la base es Infinito(positivo) y el exponente es Infinito(negativo) el resultado será 0</li>
-	 * <li>Cuando la base es 0 y exponente 0 el resultado dará 1</li>
 	 * 
 	 */
 
--- Calculadora/src/test/TestClase4Cociente.java
+++ Calculadora/src/test/TestClase4Cociente.java
@@ -18,7 +18,9 @@ import calculadora.Clase4Cociente;
 
 class TestClase4Cociente {
 
-
+	/*
+	 * Comprobacion metodo divisionReales
+	 */
 
 	@Test
 	void testDivisionRealesInfinitoPostivo() {
@@ -29,6 +31,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacion metodo divisionReales con numero negativo
+	 */
 	@Test
 	void testDivisionRealesDividendoNegativo() {
 
@@ -37,7 +42,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
-
+	/*
+	 * Comprobacion metodo divisionReales con Divisor 0
+	 */
 	@Test
 	void testDivisionRealesDivisor0() {
 
@@ -47,7 +54,20 @@ class TestClase4Cociente {
 	}
 
 	/*
-	 * Test Division enteros
+	 * Comprobacion metodo divisionesReales con ambos parameros a 0
+	 */
+	@Test
+	void testDivisionRealesParametros0() {
+
+		double obtenido = Clase4Cociente.divisionReales(0, 0);
+		double esperado = Double.NaN;
+		assertEquals(obtenido, esperado);
+	}
+
+	/*
+	 * Comprobacion metodo divisionEnteros
+	 * 
+	 * comprobacion divisonEnteros con ambos parametros a 0
 	 */
 
 	@Test
@@ -57,6 +77,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacion divisionEnteros Not-a-Number
+	 */
 	@Test
 	void testDivisionEnterosNaN() {
 		double obtenido = Clase4Cociente.divisionEnteros(Double.NaN, Double.NaN);
@@ -64,6 +87,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacion divisionEnteros Dividendo Infinito positivo.
+	 */
 	@Test
 	void testDivisionEnterosinfinitoNaN() {
 		double obtenido = Clase4Cociente.divisionEnteros(Double.POSITIVE_INFINITY, 2);
@@ -72,7 +98,17 @@ class TestClase4Cociente {
 	}
 
 	/*
-	 * Test Inverso
+	 * Comprobacion divisionEnteros Divisor 0.
+	 */
+	@Test
+	void testDivisionEnterosinfinitDivisor0() {
+		double obtenido = Clase4Cociente.divisionEnteros(8, 0);
+		double esperado = Double.POSITIVE_INFINITY;
+		assertEquals(obtenido, esperado);
+	}
+
+	/*
+	 * Comprobacion Inverso 
 	 */
 	@Test
 	void testInverso() {
@@ -81,6 +117,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacin Inverso parametro 0.
+	 */
 	@Test
 	void testInverso0() {
 		double obtenido = Clase4Cociente.inverso(0);
@@ -88,37 +127,61 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacion Inverso Not-a-Number
+	 */
 	@Test
 	void testInversoNegativo() {
-		double obtenido = Clase4Cociente.inverso(-5);
-		double esperado = -0.2;
+		double obtenido = Clase4Cociente.inverso(Double.NaN);
+		double esperado = Double.NaN;
 		assertEquals(obtenido, esperado);
 	}
-
+	
+	/*
+	 * Comprobacion Inverso Infinito Positivo
+	 */
 	@Test
-	void testInversoInfinito() {
+	void testInversoInfinitoPositivo() {
 		double obtenido = Clase4Cociente.inverso(Double.POSITIVE_INFINITY);
 		double esperado = 0;
 		assertEquals(obtenido, esperado);
 	}
+	
 	/*
-	 * Test Raiz cuadrada
+	 * Comprobacion Inverso Infinito Positivo
 	 */
-
+	@Test
+	void testInversoInfinitoNegativo() {
+		double obtenido = Clase4Cociente.inverso(Double.NEGATIVE_INFINITY);
+		double esperado = -0;
+		assertEquals(obtenido, esperado);
+	}
+	
+	
+	/*
+	 * comprobacion raizCuadrada
+	 */
 	@Test
 	void testRaizCuadrada() {
 		double obtenido = Clase4Cociente.raizCuadrada(2.5);
 		double esperado = 1.5811388300841898;
 		assertEquals(obtenido, esperado);
 	}
-	
+
+	/*
+	 * Comprobacion raizCuadrada Infinito Positivo
+	 */
 	@Test
 	void testRaizCuadradaInfinita() {
 		double obtenido = Clase4Cociente.raizCuadrada(Double.POSITIVE_INFINITY);
 		double esperado = Double.POSITIVE_INFINITY;
 		assertEquals(obtenido, esperado);
 	}
-
+	
+	
+	/*
+	 * Comprobacion raizCuadrada Infinito Negativo.
+	 */
 	@Test
 	void testRaizCuadradaInfinitaNegativa() {
 		double obtenido = Clase4Cociente.raizCuadrada(Double.NEGATIVE_INFINITY);
@@ -126,6 +189,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacion raizCuadrada 0
+	 */
 	@Test
 	void testRaizCuadrada0() {
 		double obtenido = Clase4Cociente.raizCuadrada(0);
@@ -133,6 +199,9 @@ class TestClase4Cociente {
 		assertEquals(obtenido, esperado);
 	}
 
+	/*
+	 * Comprobacion raizCuadrada Numero Negativo.
+	 */
 	@Test
 	void testRaizCuadradaNegativo() {
 		double obtenido = Clase4Cociente.raizCuadrada(-4);
