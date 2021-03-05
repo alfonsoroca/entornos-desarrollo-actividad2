3e2a202ef929fac58ab94e8e5d4c5f9ef6b21981
morenoer <rafaelmestevez@gmail.com>
2021-03-05 14:30:15 +0100

    modificacion codigo de clase y testclase

--- Calculadora/src/calculadora/Clase1Suma.java
+++ Calculadora/src/calculadora/Clase1Suma.java
@@ -2,6 +2,7 @@ package calculadora;
 
 import java.util.Scanner;
 
+
 /**
  * <li>Esta clase permite realizar diferentes operaciones de tipo suma:</li>
  * <ul>
@@ -11,20 +12,25 @@ import java.util.Scanner;
  * <li>Suma acumulativa</li>
  * </ul>
  * 
- * <li> Casos Especiales </li>
+ * <li>Casos Especiales</li>
  * <ul>
- * <li>Cualquier número entero o real al que se le suma +infinito da como resultado +infinito</li>
- * <li>Cualquier número entero o real al que se le suma -infinito da como resultado -infinito</li>
- * <li>Cualquier suma empleando en los sumandos valores de +infinito y -infinito dan como resultado un valor NaN </li>
+ * <li>Cualquier número entero o real al que se le suma +infinito da como
+ * resultado +infinito</li>
+ * <li>Cualquier número entero o real al que se le suma -infinito da como
+ * resultado -infinito</li>
+ * <li>Cualquier suma empleando en los sumandos valores de +infinito y -infinito
+ * dan como resultado un valor NaN</li>
  * </ul>
  * <br>
- * <b>ATENCIóN:</b> Para operar con números decimales usar la ","!!<p>
- * <b>ATENCIÓN</b> No emplear numeros negativos para realizar los calculos!!!<p>
+ * <b>ATENCIóN:</b> Para operar con números decimales usar la ","!!
+ * <p>
+ * <b>ATENCIÓN</b> No emplear numeros negativos para realizar los calculos!!!
+ * <p>
  * 
  * @author Alfonso, Estanislao, Rafael
  * @since 24/01/2021
  * 
- * Clase realizada por @Rafa tras la unificación de grupos
+ *        Clase realizada por @Rafa tras la unificación de grupos
  */
 
 public class Clase1Suma {
@@ -54,34 +60,24 @@ public class Clase1Suma {
 	 */
 
 	public static double sumaNumReal(double sum1, double sum2) {
-		// Utilizamos tipos float para controlar el overflow
+		// Utilizamos tipos float para controlar el que no sebrepase el valor
 		float sumador1 = (float) sum1;
 		float sumador2 = (float) sum2;
 		float resultado = sumador1 + sumador2;
 
-		if (resultado > Double.MAX_VALUE) {
-			System.out.println("Error producido por superar el valor máximo permitido:");
-			return Double.MAX_VALUE;
-
-		} else if (resultado < -Double.MAX_VALUE) {
-			System.out.println("Error producido por superar el valor mínimo permitido:");
-			return -Double.MAX_VALUE;
-
-		} else {
-			return (double) resultado;
-		}
-
+		return (double) resultado;
+		
 	}
-
-	
 
 	/**
 	 * <b>Método sumaNumEnt</b> que retorna la suma de dos números enteros.
+	 * 
 	 * @param sum1 Número entero correspondiente con el primer sumando
 	 * @param sum2 Número entero correspondiente con el segundo sumando
-	 * @return Retorna la suma de los dos sumandos introducidos según la fórmula: sum1 + sum2
+	 * @return Retorna la suma de los dos sumandos introducidos según la fórmula:
+	 *         sum1 + sum2
 	 * 
-	 * 	       <ul>
+	 *         <ul>
 	 *         <li><b>ATENCIÓN</b> No emplear <b>números negativos</b> para realizar
 	 *         los calculos!!!</li>
 	 *         </ul>
@@ -97,7 +93,7 @@ public class Clase1Suma {
 
 	public static int sumaNumEnt(int sum1, int sum2) {
 
-		// Utilizamos tipos float para controlar el overflow
+		// Utilizamos tipos float para controlar el que no sebrepase el valor
 		float sumador1 = (float) sum1;
 		float sumador2 = (float) sum2;
 		float resultado = sumador1 + sumador2;
@@ -140,71 +136,58 @@ public class Clase1Suma {
 	 */
 
 	public static double sumaNumReal(double sum1, double sum2, double sum3) {
-		// Utilizamos tipos float para controlar el overflow
-				float sumador1 = (float) sum1;
-				float sumador2 = (float) sum2;
-				float sumador3 = (float) sum3;
-				float resultado = sumador1 + sumador2 + sumador3;
+		// Utilizamos tipos float para controlar el que no sebrepase el valor
+		float sumador1 = (float) sum1;
+		float sumador2 = (float) sum2;
+		float sumador3 = (float) sum3;
+		float resultado = sumador1 + sumador2 + sumador3;
 
-				if (resultado > Double.MAX_VALUE) {
-					System.out.println("Error producido por superar el valor máximo permitido:");
-					return Double.MAX_VALUE;
-
-				} else if (resultado < -Double.MAX_VALUE) {
-					System.out.println("Error producido por superar el valor mínimo permitido:");
-					return -Double.MAX_VALUE;
-
-				} else
-					return (double) resultado;
+		return (double) resultado;
 	}
-	
+
 	/**
 	 * Método para sumar acumulativamente diferentes números reales
-	 * @param sumador Número real que se quiere añadir al sumatorio
-	 * @param acumulador almacenamos el valor del sumatorio de todos los 
-	 * números introducidos
-	 *         <ul>
-	 *         <li><b>ATENCIóN:</b> Para operar con números decimales usar la
-	 *         ","!!</li>
-	 *         <li><b>ATENCIÓN</b> No emplear <b>números negativos</b> para realizar
-	 *         los calculos!!!</li>
-	 *         </ul>
-	 *         <ul>
-	 *         <li>Cualquier número entero o real al que se le suma +infinito da
-	 *         como resultado +infinito</li>
-	 *         <li>Cualquier número entero o real al que se le suma -infinito da
-	 *         como resultado -infinito</li>
-	 *         <li>Cualquier suma empleando en los sumandos valores de +infinito y
-	 *         -infinito dan como resultado un valor NaN</li>
-	 *         <li> Cualquier cálculo que supere el valor del MAX_VALUE y el MIN_VALUE dara mensaje de error y retornará el valos MAX/MIN_Value
-	 *         </ul>
+	 * 
+	 * @param sumador    Número real que se quiere añadir al sumatorio
+	 * @param acumulador almacenamos el valor del sumatorio de todos los números
+	 *                   introducidos
+	 *                   <ul>
+	 *                   <li><b>ATENCIóN:</b> Para operar con números decimales usar
+	 *                   la ","!!</li>
+	 *                   <li><b>ATENCIÓN</b> No emplear <b>números negativos</b>
+	 *                   para realizar los calculos!!!</li>
+	 *                   </ul>
+	 *                   <ul>
+	 *                   <li>Cualquier número entero o real al que se le suma
+	 *                   +infinito da como resultado +infinito</li>
+	 *                   <li>Cualquier número entero o real al que se le suma
+	 *                   -infinito da como resultado -infinito</li>
+	 *                   <li>Cualquier suma empleando en los sumandos valores de
+	 *                   +infinito y -infinito dan como resultado un valor NaN</li>
+	 *                   <li>Cualquier cálculo que supere el valor del MAX_VALUE y
+	 *                   el MIN_VALUE dara mensaje de error y retornará el valos
+	 *                   MAX/MIN_Value
+	 *                   </ul>
 	 */
 
-	public static double sumatorio() {
-		double acumulado = 0;
-		double sumador = 0;
-		//pedimos un valor por teclado y entra en el bucle siempre que no sea 0
-		System.out.println("Inserta numeros para realizar una suma acumulativa. \n Si introduces 0 acabara, para indicar decimales utilizar ','\n");
-		Scanner sc= new Scanner(System.in);
-			acumulado = sumador = sc.nextDouble();
-		//mientras el valor añadido no sea 0, se ejecuta el bucle añadiendo todos los valores introducicos
-		while(sumador != 0) {
-			System.out.println("Siguiente valor. \nRecuerda: si introduces 0 acabara y ',' para decimales\n");
-			Scanner valor= new Scanner(System.in);
-				sumador = valor.nextDouble();
-				acumulado += sumador;
-		}
-		sc.close();
-		if (acumulado > Double.MAX_VALUE) {
-			System.out.println("Error producido por superar el valor máximo permitido:");
-			return Double.MAX_VALUE;
+	static double sumatorio= 0;
+	public static double sumatorio(double sumador) {
+		sumatorio += sumador;
+		return sumatorio;
+	}
 
-		} else if (acumulado < -Double.MAX_VALUE) {
-			System.out.println("Error producido por superar el valor mínimo permitido:");
-			return -Double.MAX_VALUE;
+	/**
+	 * Devuelve el valor actual del campo {@link Clase1Suma#acumulador}
+	 * @return El valor del campo {@link Clase1Suma#acumulador}
+	 */
+	public static double getAcumulador() {
+		return sumatorio;
+	}
 
-		} else
-			//retorna el valor del valor acumuado
-			return (double) acumulado;		
+	/**
+	 * Pone el valor del campo {@link Clase1Suma#acumulador} a 0.
+	 */
+	public static void resetAcumulador() {
+		sumatorio = 0;
 	}
 }
--- Calculadora/src/test/TestClase2Resta.java
+++ Calculadora/src/test/TestClase2Resta.java
@@ -23,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 		@Test
 		
+	//Inicio test de la  clase, primer método  restaNumReal
 		
 		//test para comprobar la resta de dos valores reales
 		 void testrestaNumReal() {
@@ -32,6 +33,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 			
 		}
 		
+		//test para valor Double.MAX_VALUE y restarle 1 numero que lo cargue
+		void testrestaNumReal(){
+			double rest1=Clase2Resta.restaNumRal(Double.MAX_VALUE, -5);
+			double rest1esperado = Double.MAX_VALUE;
+			assertEqual(rest1,rest1esperado);
+		}
+		
+		//test para valor Double.MIN_VALUE y restarle un valor que los cargue
+		void testrestaNumReal(){
+			double rest1=Clase2Resta.restaNumRal(Double.MIN_VALUE, 5);
+			double rest1esperado = Double.MIN_VALUE;
+			assertEqual(rest1,rest1esperado);
+		}
+		
+		//test usando 1 valor como infinito y otro valor que lo resta
+		void testrestaNumReal(){
+			double rest1=Clase2Resta.restaNumReal(Double.POSITIVE_INFINITY, -1);
+			double rest1esperado = Double.POSITIVE_INFINITY;
+			assertEquals(rest1,rest1esperado);
+		}
+		//test usando 1 valor como menos infinito y otro valor que lo resta
+		void testrestaNumReal() {
+			double rest1 = Clase2Resta.restaNumReal(Double.NEGATIVE_INFINITY, -1);
+			double rest1esperado = Double.NEGATIVE_INFINITY;
+			assertEquals(rest1, rest1esperado);
+		}
+		
+		
+		
+	//test segundo método de la clase resta, restaNumEnt
 		@Test
 		//test para verificar la resta de dos numeros enteros
 		void testrestaNumEnt() {
@@ -39,6 +70,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 			int rest1esperado = 30;
 			assertEquals(rest1, rest1esperado);
 		}
+		//test para valor Double.MAX_VALUE y restarle 1 numero que lo cargue
+		void testrestaNumReal(){
+			int rest1=Clase2Resta.restaNumRal(Double.MAX_VALUE, -5);
+			int rest1esperado = Double.MAX_VALUE;
+			assertEqual(rest1,rest1esperado);
+		}
+		
+		//test para valor Double.MIN_VALUE y restarle un valor que los cargue
+		void testrestaNumReal(){
+			int rest1=Clase2Resta.restaNumRal(Double.MIN_VALUE, 5);
+			int rest1esperado = Double.MIN_VALUE;
+			assertEqual(rest1,rest1esperado);
+		}
+		
+		//test usando 1 valor como infinito y otro valor que lo resta
+		void testrestaNumReal(){
+			int rest1=Clase2Resta.restaNumReal(Double.POSITIVE_INFINITY, -1);
+			int rest1esperado = Double.POSITIVE_INFINITY;
+			assertEquals(rest1,rest1esperado);
+		}
+		//test usando 1 valor como menos infinito y otro valor que lo resta
+		void testrestaNumReal() {
+			int rest1 = Clase2Resta.restaNumReal(Double.NEGATIVE_INFINITY, -1);
+			int rest1esperado = Double.NEGATIVE_INFINITY;
+			assertEquals(rest1, rest1esperado);
+		}
+		
+	//inicio test del tercer metodo restaNumReala3
 		
 		@Test
 		//test para verificar la resta de tres numeros reales
@@ -48,6 +107,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 			assertEquals(restade3, restade3esperado);
 			
 		}
+		
+		//test para valor Double.MAX_VALUE y restarle 1 numero que lo cargue
+		void testrestaNumReal(){
+			double rest1=Clase2Resta.restaNumRal(Double.MAX_VALUE, -5, 2);
+			double rest1esperado = Double.MAX_VALUE;
+			assertEqual(rest1,rest1esperado);
+		}
+		
+		//test para valor Double.MIN_VALUE y restarle un valor que los cargue
+		void testrestaNumReal(){
+			double rest1=Clase2Resta.restaNumRal(Double.MIN_VALUE, 5, 3);
+			double rest1esperado = Double.MIN_VALUE;
+			assertEqual(rest1,rest1esperado);
+		}
+		
+		//test usando 1 valor como infinito y otro valor que lo resta
+		void testrestaNumReal(){
+			double rest1=Clase2Resta.restaNumReal(Double.POSITIVE_INFINITY, -1, 2);
+			double rest1esperado = Double.POSITIVE_INFINITY;
+			assertEquals(rest1,rest1esperado);
+		}
+		//test usando 1 valor como menos infinito y otro valor que lo resta
+		void testrestaNumReal() {
+			double rest1 = Clase2Resta.restaNumReal(Double.NEGATIVE_INFINITY, -1, 2);
+			double rest1esperado = Double.NEGATIVE_INFINITY;
+			assertEquals(rest1, rest1esperado);
+		}
+		
+	
+		
 		
 		@Test
 		//test para verificar el 
